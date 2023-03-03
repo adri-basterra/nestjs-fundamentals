01e6d90f57b927d16bca919f2e00f26804e06456
adri-st <abasteru@emeal.nttdata.com>
2023-03-03 12:11:08 +0100

    feat: basic comments & functions

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core'; // exposes a few static methods that allow creating an application instance
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule); // returns an application object with `INestApplication` interface
+  //const app = await NestFactory.create<NestExpressApplication>(AppModule); // only if actually want to access the underlying platform API.
   await app.listen(3000);
 }
 bootstrap();
